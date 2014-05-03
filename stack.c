09dc16f8036dcdff2bd23563293327cfb4046296
bryan0806 <chunwei.liu@gmail.com>
2014-05-03 13:38:38 +0800

    add the first function "isStackEmpty"

---
+++ stack.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define STACK_SIZE 10
+
+int isStackEmpty(int *stack,int top){
+	if(top==-1) return 1;
+	else return 0;
+
+}
+
+int main(void){
+	
+	int stack[STACK_SIZE];
+	
+	int i,top=-1;
+
+	// below is for testing 
+	for(i=0;i<STACK_SIZE;i++){
+		printf("%3d",stack[i]);
+	}
+	
+	if(isStackEmpty(stack,top))
+		printf("stack is empty\n");
+	else
+		printf("stack is not empty\n");
+		
+		
+
+	return 0;
+}
